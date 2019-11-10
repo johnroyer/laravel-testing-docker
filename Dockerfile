1653380da618d126885b3cf814a41ec91b117417
Zero <john.royer@gmail.com>
2019-11-10 13:35:57 +0800

    Install Mysql non-interactive

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM php:7.3.11-cli
 
-RUN apt-get update && apt-get install -y wget  apt-utils bash nano less
+RUN apt-get update && apt-get install -y wget  apt-utils bash nano less debconf
 
 # install MySQL 5.7
 COPY mysql-download.sh /tmp/mysql-download.sh
+COPY install.sh /tmp/install.sh
+RUN bash /tmp/mysql-download.sh
+RUN bash /tmp/install.sh
 
 #RUN find / -name '*mysql*' -type d
 
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd /tmp
+ls
+export DEBIAN_FRONTEND="noninteractive"
+export PASSWD="root"
+debconf-set-selections <<< "mysql-community-server/root-pass password root"
+debconf-set-selections <<< "mysql-community-server/root-pass password root"
+dpkg -i mysql-common_5.7.27-1ubuntu18.04_amd64.deb  
+dpkg -i libmysqlclient20_5.7.27-1ubuntu18.04_amd64.deb 
+dpkg -i mysql-community-server_5.7.27-1ubuntu18.04_amd64.deb
+dpkg -i mysql-community-client_5.7.27-1ubuntu18.04_amd64.deb
+dpkg -i mysql-client_5.7.27-1ubuntu18.04_amd64.deb 
+dpkg -i mysql-server_5.7.27-1ubuntu18.04_amd64.deb 
+apt-get install -f -y mysql-client mysql-server
+apt --fix-broken -y install 
